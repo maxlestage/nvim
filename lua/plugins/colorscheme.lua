1ff16cc90416fd612bea90e8b7e8d293e771ceeb
maxlestage <maxlestage@icloud.com>
2024-02-03 20:54:43 +0100

    first commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,92 @@
+-- return {
+--   -- add gruvbox
+--   { "rebelot/kanagawa.nvim" },
+
+--   -- Configure LazyVim to load gruvbox
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "kanagawa-wave",
+--     },
+--   }
+-- }
+
+-- return {
+
+--   { "ribru17/bamboo.nvim" },
+
+
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "bamboo",
+--     },
+--   }
+-- }
+
+-- return {
+
+--   { "nanotech/jellybeans.vim" },
+
+
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "jellybeans",
+--     },
+--   }
+-- }
+
+
+-- return {
+
+--   { "Mofiqul/vscode.nvim" },
+
+
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "vscode",
+--     },
+--   }
+-- }
+
+-- return {
+
+--   { "deparr/tairiki.nvim" },
+
+
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "tairiki",
+--     },
+--   }
+-- }
+
+-- return {
+
+--   { "olimorris/onedarkpro.nvim" },
+
+
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "onedark",
+--     },
+--   }
+-- }
+
+
+return {
+
+  { "marko-cerovac/material.nvim" },
+
+  {
+    "LazyVim/LazyVim",
+
+    opts = {
+      colorscheme = "material-darker",
+    },
+  }
+}
